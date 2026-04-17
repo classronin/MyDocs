65d1d391a2afed73b30a3126fda80cc51d6c6032
classronin <classronin@msn.com>
2026-04-17 13:10:34 +0800

    Update PNG.bat

--- Docs/ImageMagick/PNG.bat
+++ Docs/ImageMagick/PNG.bat
@@ -1,8 +1,2 @@
 @echo off
-if "%~1"=="" (
-    echo 用法: png ^<动作^> [文件...]
-    echo 示例: png 右旋 1.jpg
-    echo       png 批量
-    exit /b 1
-)
 uv run --project "%SCRIPTS%\im" python "%SCRIPTS%\im\im.py" png %*
